594ad72f9a51ad04e346def59ab4bd5365b36670
Sandeep B <sandeepb@reflections.local>
2021-01-26 17:14:38 +0530

    new build0

--- projects/sand-simple-select/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of sand-simple-select
- */
-
-export * from './lib/service/sand-simple-select.service';
-export * from './lib/sand-simple-select/sand-simple-select.component';
-export * from './lib/sand-simple-select.module';
