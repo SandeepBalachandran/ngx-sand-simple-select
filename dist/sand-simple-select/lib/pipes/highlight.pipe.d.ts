8e60df7dc70caab9f3dd25ffccf1dae3839c05ac
Sandeep Balachandran <sandeepbalachandran2@gmail.com>
2021-02-15 19:58:33 +0530

    Delete dist directory

--- dist/sand-simple-select/lib/pipes/highlight.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class HighlightSearch implements PipeTransform {
-    transform(value: any[], key: any, query: any): any;
-    static ɵfac: i0.ɵɵFactoryDef<HighlightSearch, never>;
-    static ɵpipe: i0.ɵɵPipeDefWithMeta<HighlightSearch, "highlight">;
-}
--- dist/sand-simple-select/sand-simple-select.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="sand-simple-select" />
-export * from './public-api';
