fc7b43a1aca2dfa35fe9240ea73f81bb2cbfe0b1
Sandeep Balachandran <sandeepbalachandran2@gmail.com>
2021-02-15 19:58:57 +0530

    Delete dist directory

--- dist/sand-simple-select/lib/pipes/filter.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class SelectFilterPipe implements PipeTransform {
-    transform(array: any[], column: string, query: string): any;
-    static ɵfac: i0.ɵɵFactoryDef<SelectFilterPipe, never>;
-    static ɵpipe: i0.ɵɵPipeDefWithMeta<SelectFilterPipe, "selectFilter">;
-}
